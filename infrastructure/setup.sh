de8fe0b5e6d1a446f46d220c85d771f900ee459a
Mustafa RZ <323mustafa2rz@gmsil.com>
2023-08-24 01:01:59 +0300

    add setup.sh file

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -8,3 +8,13 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 
 sudo apt-get install  nodejs curl -y
 
+cd /home/app
+git clone https://github.com/Mustafazaareer/book-app-mustafa.git app 
+cd app && npm install
+npm run build
+
+sudo mv ./infrastructure/app.service /etc/systemd/system/
+sudo systemctl deamon-reload
+sudo systemctl enable app.service
+sudo reboot
+
