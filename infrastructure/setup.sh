2c9755ac3c34b771390b6f4e62b0da64b3bdad43
Mustafa Raed Muflih Zaareer <105263355+Mustafazaareer@users.noreply.github.com>
2023-08-24 05:06:57 +0300

    Update setup.sh

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+set -e
+
 sudo apt update
 sudo apt upgrade -y
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 
-#install nodejs
+sudo apt install nodejs curl -y
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - 
-
-sudo apt-get install  nodejs curl -y
-
-cd /home/app
-git clone https://github.com/Mustafazaareer/book-app-mustafa.git app 
+cd /home/mustafa
+git clone https://github.com/khaledez/Books-API-Shadi.git app
 cd app && npm install
 npm run build
 
@@ -17,4 +16,3 @@ sudo mv ./infrastructure/app.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable app.service
 sudo reboot
-
